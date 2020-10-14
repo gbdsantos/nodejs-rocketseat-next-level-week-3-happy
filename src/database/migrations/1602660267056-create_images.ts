85b5dae2082eebafe1d5fd80e00c643b159debed
Guilherme Bezerra <gbsantos.it@gmail.com>
2020-10-14 04:31:25 -0300

    :card_file_box: database(backend): create images migration

---
+++ src/database/migrations/1602660267056-create_images.ts
@@ -0,0 +1,43 @@
+import  {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createImages1602660267056 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'images',
+      columns: [
+        {
+          name: 'id',
+          type: 'integer',
+          unsigned: true, // Not permit negative numbers
+          isPrimary: true,
+          isGenerated: true, // this column is automatic generate 
+          generationStrategy: 'increment',
+        },
+        {
+          name: 'path',
+          type: 'varchar',
+        },
+        {
+          name: 'orphanage_id',
+          type: 'integer',
+        }
+      ],
+      // Relationship One to Many (1-N)
+      foreignKeys: [
+        {
+          name: 'ImageOrphanage',
+          columnNames: ['orphanage_id'],
+          referencedTableName: 'orphanages',
+          referencedColumnNames: ['id'],
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE'
+        }
+      ]
+    }))
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('images');
+  }
+}
