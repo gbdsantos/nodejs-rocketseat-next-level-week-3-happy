f5d67cdd0e440b36ad12f298fb6a8d8c514971cf
Guilherme Bezerra <gbsantos.it@gmail.com>
2020-10-14 05:42:21 -0300

    :star: feat(backend): serve static files
    = Images storage on the server.

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 import './database/connection';
 import routes from './routes';
@@ -8,5 +9,8 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+// 💡 DOCS:  SERVE STATIC FILES STORAGE ON THE SERVER
+app.use('/uploads', express.static(path.join(__dirname, '..', '/uploads')));
 
 app.listen(3333);
