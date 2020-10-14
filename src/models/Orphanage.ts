d2f27854d59b8123ae82bbbc9260248149e6aeeb
Guilherme Bezerra <gbsantos.it@gmail.com>
2020-10-14 04:57:58 -0300

    :card_file_box: database(backend): create images model and relationship one to many
    - Define relationship between orphanages and images. One image belongs/has to one orphanage(1-1), one orphanage belongs/has many images(1-N),

--- src/models/Orphanage.ts
+++ src/models/Orphanage.ts
@@ -1,28 +1,42 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import Image from './Image';
 
 @Entity('orphanages')
-export default class Orphanages { 
+export default class Orphanages {
   @PrimaryGeneratedColumn('increment')
   id: number;
-  
+
   @Column()
   name: string;
-  
+
   @Column()
   latitude: number;
-  
+
   @Column()
   longitude: number;
-  
+
   @Column()
   about: string;
-  
+
   @Column()
   instructions: string;
-  
+
   @Column()
   opening_hours: string;
-  
+
   @Column()
   open_on_weekends: boolean;
+
+  // this field does not exist on database
+  @OneToMany(() => Image, (image) => image.orphanage, {
+    cascade: ['insert', 'update'],
+  })
+  @JoinColumn({ name: 'orphanage_id' })
+  images: Image[];
 }
