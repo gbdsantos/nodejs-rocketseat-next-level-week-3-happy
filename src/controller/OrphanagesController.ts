0bcd604abd55da41230d9c6577c4643665c03551
Guilherme Bezerra <gbsantos.it@gmail.com>
2020-10-15 01:25:33 -0300

    :pencil: update(backend): convert the variable with true value as a string type to boolean type

--- src/controller/OrphanagesController.ts
+++ src/controller/OrphanagesController.ts
@@ -36,7 +36,7 @@ class OrphanagesController {
       about,
       instructions,
       opening_hours,
-      open_on_weekends,
+      open_on_weekends: open_on_weekends === 'true',
       images,
     };
 
